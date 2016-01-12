a8cc3ddbe30964bf31cf32e4e70bd39bc869c380
Martin95056 <email_martin@abv.bg>
2016-01-12 12:02:06 +0200

    Added panda_to_dict()

--- panda.py
+++ panda.py
@@ -1,34 +1,37 @@
 class Panda:
-	def __init__(self, name, email, gender):
-		self.name = name
-		self.email = email
-		self.gender = gender
+    def __init__(self, name, email, gender):
+        self.name = name
+        self.email = email
+        self.gender = gender
 
-	def __str__(self):
-		return '{}, {}, {}'.format(self.name, self.email, self.gender)
+    def __str__(self):
+        return '{}, {}, {}'.format(self.name, self.email, self.gender)
 
-    #def __repr__(self):
-     #   return "Panda('{}', '{}', '{}')".format(self.name, self.email, self.gender)
+        #def __repr__(self):
+        #   return "Panda('{}', '{}', '{}')".format(self.name, self.email, self.gender)
 
-	def __eq__(self, other):
-		return self.name == other.name and self.email == other.email and self.gender == other.gender
+    def __eq__(self, other):
+        return self.name == other.name and self.email == other.email and self.gender == other.gender
 
-	def __hash__(self):
-		return hash(self.name + 'pandapanda')
+    def __hash__(self):
+        return hash(self.name + 'pandapanda')
 
-	def name(self):
-		return self.name
+    def name(self):
+        return self.name
 
-	def email(self):
-		if self.email.split('@')[1] == 'pandamail.com':
-			return self.email
-		return False
+    def email(self):
+        if self.email.split('@')[1] == 'pandamail.com':
+            return self.email
+        return False
 
-	def gender(self):
-		return self.gender
+    def gender(self):
+        return self.gender
 
-	def isMale(self):
-		return self.gender == "male"
+    def isMale(self):
+        return self.gender == "male"
 
-	def isFemale(self):
-		return self.gender == "female"
+    def isFemale(self):
+        return self.gender == "female"
+
+    def panda_to_dict(self):
+        return str(self.__dict__)
